e19d281e4f22360ffd96b292c646d8151d5a2952
Louis <I526784@student.fontys.nl>
2024-11-15 11:28:24 +0100

    Kind meeverzekeren (Angelina) header sticky + hight fix receipt (Louis) Andere verzekeringen (Joelle)

--- scripts.js
+++ scripts.js
@@ -17,3 +17,35 @@ function changeFontSize(increment) {
 // Add event listeners to the buttons
 buttonDown.addEventListener('click', () => changeFontSize(-4));
 buttonUp.addEventListener('click', () => changeFontSize(4));
+
+
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    const navTopBar = document.querySelector('.nav-top-bar');
+    const navBottomBar = document.querySelector('.nav-bottom-bar');
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+        navTopBar.classList.add('scrolled');
+        navBottomBar.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+        navTopBar.classList.remove('scrolled');
+        navBottomBar.classList.remove('scrolled');
+    }
+});
+
+<script>
+        // JavaScript to handle radio button selection
+        document.getElementById("submitButton").addEventListener("click", function() {
+            // Get all radio buttons in the form
+            const radioButtons = document.querySelectorAll('input[name="plan"]');
+            let selectedValue = "";
+
+            // Loop through the radio buttons to find the checked one
+            for (const radioButton of radioButtons) {
+                if (radioButton.checked) {
+                    selectedValue = radioButton.value;
+                    break;
+                }
+            }
+</script>
